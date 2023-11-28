1de4c1efe36a7fc6cd5ea1e24b70461dfcdc3e6e
adilet <adik.it22run@gmail.com>
2023-11-28 20:05:05 +0600

    '28.11.23action'

--- src/pages/action/action.js
+++ src/pages/action/action.js
@@ -1,11 +1,163 @@
 import React from 'react';
+import './action.scss'
+import Rect from './image/Rectangle 77.png'
+import Rect1 from './image/Rect1.png'
+import Rect2 from './image/Rect2.png'
+import Rect3 from './image/Rect3.png'
+import Rect4 from './image/Rect4.png'
+import Rect5 from './image/Rect5.png'
+import Recct6 from './image/Recct6.png'
+import Rect7 from './image/Rect7.png'
+import Rect8 from './image/Rect8.png'
+import Rect9 from './image/Rect9.png'
+import Rect10 from './image/Rect 10.png'
+import Rect11 from './image/Rect11.png'
 
 const Action = () => {
     return (
-        <>
-           action
-        </>
+        <action className='action'>
+            <div className="container">
+                <h2>Акции</h2>
+                <div className='action__kupony'>
+                    <div className="action__cards">
+                        <img src={Rect} alt=""/>
+                        <h3>Дарим кибер-призы</h3>
+                        <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                            от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                            от Bite. А также станьте автоматическим участником розыгрыша
+                            игровых ключей и больших пицц 29 июня.
+                        </p>
+                        <button>Посмотреть</button>
+                    </div>
+
+                    <div className="action__cards">
+                        <img src={Rect1} alt=""/>
+                        <h3>Дарим кибер-призы</h3>
+                        <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                            от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                            от Bite. А также станьте автоматическим участником розыгрыша
+                            игровых ключей и больших пицц 29 июня.
+                        </p>
+                        <button>Посмотреть</button>
+                    </div>
+
+                    <div className="action__cards">
+                        <img src={Rect2} alt=""/>
+                        <h3>Дарим кибер-призы</h3>
+                        <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                            от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                            от Bite. А также станьте автоматическим участником розыгрыша
+                            игровых ключей и больших пицц 29 июня.
+                        </p>
+                        <button>Посмотреть</button>
+                    </div>
+                </div>
+                <div className='action__kupony'>
+                    <div className="action__cards">
+                        <img src={Rect3} alt=""/>
+                        <h3>Дарим кибер-призы</h3>
+                        <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                            от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                            от Bite. А также станьте автоматическим участником розыгрыша
+                            игровых ключей и больших пицц 29 июня.
+                        </p>
+                        <button>Посмотреть</button>
+                    </div>
+
+                    <div className="action__cards">
+                        <img src={Rect4} alt=""/>
+                        <h3>Дарим кибер-призы</h3>
+                        <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                            от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                            от Bite. А также станьте автоматическим участником розыгрыша
+                            игровых ключей и больших пицц 29 июня.
+                        </p>
+                        <button>Посмотреть</button>
+                    </div>
+
+                    <div className="action__cards">
+                        <img src={Rect5} alt=""/>
+                        <h3>Дарим кибер-призы</h3>
+                        <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                            от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                            от Bite. А также станьте автоматическим участником розыгрыша
+                            игровых ключей и больших пицц 29 июня.
+                        </p>
+                        <button>Посмотреть</button>
+                    </div>
+                </div><div className='action__kupony'>
+                <div className="action__cards">
+                    <img src={Recct6} alt=""/>
+                    <h3>Дарим кибер-призы</h3>
+                    <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                        от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                        от Bite. А также станьте автоматическим участником розыгрыша
+                        игровых ключей и больших пицц 29 июня.
+                    </p>
+                    <button>Посмотреть</button>
+                </div>
+
+                <div className="action__cards">
+                    <img src={Rect7} alt=""/>
+                    <h3>Дарим кибер-призы</h3>
+                    <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                        от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                        от Bite. А также станьте автоматическим участником розыгрыша
+                        игровых ключей и больших пицц 29 июня.
+                    </p>
+                    <button>Посмотреть</button>
+                </div>
+
+                <div className="action__cards">
+                    <img src={Rect8} alt=""/>
+                    <h3>Дарим кибер-призы</h3>
+                    <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                        от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                        от Bite. А также станьте автоматическим участником розыгрыша
+                        игровых ключей и больших пицц 29 июня.
+                    </p>
+                    <button>Посмотреть</button>
+                </div>
+            </div><div className='action__kupony'>
+                <div className="action__cards">
+                    <img src={Rect9} alt=""/>
+                    <h3>Дарим кибер-призы</h3>
+                    <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                        от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                        от Bite. А также станьте автоматическим участником розыгрыша
+                        игровых ключей и больших пицц 29 июня.
+                    </p>
+                    <button>Посмотреть</button>
+                </div>
+
+                <div className="action__cards">
+                    <img src={Rect10} alt=""/>
+                    <h3>Дарим кибер-призы</h3>
+                    <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                        от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                        от Bite. А также станьте автоматическим участником розыгрыша
+                        игровых ключей и больших пицц 29 июня.
+                    </p>
+                    <button>Посмотреть</button>
+                </div>
+
+                <div className="action__cards">
+                    <img src={Rect11} alt=""/>
+                    <h3>Дарим кибер-призы</h3>
+                    <p>Вот так ачивка! Закажите Кибер-комбо и получите доступ к играм
+                        от MY.GAMES, а еще кокосовый батончик и шоколадное печенье «Cyber»
+                        от Bite. А также станьте автоматическим участником розыгрыша
+                        игровых ключей и больших пицц 29 июня.
+                    </p>
+                    <button>Посмотреть</button>
+                </div>
+            </div>
+            </div>
+        </action>
+
+
     );
 };
+
 
 export default Action;
